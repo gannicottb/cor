7a8ecfed7639e922e000b892f6b3d502c4e231ff
Brandon Gannicott <brandon.gannicott@gmail.com>
2014-04-01 21:26:07 -0400

    Changed timezone for development environment. Added functional scopes to BloodOxygen and Weight

--- cor-dashboard/app/models/weight_reading.rb
+++ cor-dashboard/app/models/weight_reading.rb
@@ -7,6 +7,6 @@ class WeightReading < ActiveRecord::Base
 		#return most current reading
 
 	end
-	scope :last_week, -> {where(:reading_time, 1.week.ago .. Time.now)}
-	
+	scope :last_week, -> {where(reading_time: 1.week.ago .. Time.now)}
+	scope :last_2_weeks, -> {where(reading_time: 2.weeks.ago .. Time.now)}
 end
