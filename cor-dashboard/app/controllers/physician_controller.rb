4bd395969478adf80628cebe82be680434e80beb
Jie Guan <jie.s.guan@gmail.com>
2014-04-21 22:35:33 -0400

    implementing linking between physician/summary and physician/detail

--- cor-dashboard/app/controllers/physician_controller.rb
+++ cor-dashboard/app/controllers/physician_controller.rb
@@ -36,25 +36,26 @@ class PhysicianController < ApplicationController
 	end
 
 	def detail
-    @physician = Physician.take
-    @patients = @physician.patients
-    patient = Patient.take
-    bundle = patient.weight
-    @threshold_wt = bundle[:threshold]
-    @values_wt = bundle[:values]
-    bundle = patient.blood_oxygen
-    @threshold_bo = bundle[:threshold]
-    @values_bo = bundle[:values]
-    bundle = patient.heart_rate
-    @threshold_hr = bundle[:threshold]
-    @values_hr = bundle[:values]
-    #bundle = patient.sodium
-    #@threshold_so = bundle[:threshold]
-    #@values_so = bundle[:values]
-    bundle = patient.blood_pressure
-    @threshold_bp = bundle[:threshold]
-    @values_bp = bundle[:values]
-  end
+		@metric = params[:metric]
+	    @physician = Physician.take
+	    @patients = @physician.patients
+	    patient = Patient.take
+	    bundle = patient.weight
+	    @threshold_wt = bundle[:threshold]
+	    @values_wt = bundle[:values]
+	    bundle = patient.blood_oxygen
+	    @threshold_bo = bundle[:threshold]
+	    @values_bo = bundle[:values]
+	    bundle = patient.heart_rate
+	    @threshold_hr = bundle[:threshold]
+	    @values_hr = bundle[:values]
+	    #bundle = patient.sodium
+	    #@threshold_so = bundle[:threshold]
+	    #@values_so = bundle[:values]
+	    bundle = patient.blood_pressure
+	    @threshold_bp = bundle[:threshold]
+	    @values_bp = bundle[:values]
+  	end
 
 
 end
