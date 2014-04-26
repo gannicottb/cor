936d41493d303bccccc6bbef807ebc58575ca277
mukho1708 <mukho1708@yahoo.com>
2014-04-26 14:28:48 -0400

    Dry run changes patient working

--- cor-dashboard/app/models/ema.rb
+++ cor-dashboard/app/models/ema.rb
@@ -1,6 +1,12 @@
 class Ema < ActiveRecord::Base
 	attr_accessible :patient_id, :temperature, :sodium_level, :quality_of_sleep, :reading_time
 
+  if Rails.env.production?
+    self.table_name = "ema"
+    alias_attribute :sodium_level, :Sodium
+    alias_attribute :reading_time, :Reading_time
+  end
+
   scope :last_week, -> {where(reading_time: 1.week.ago .. Time.now)}
   scope :last_2_weeks, -> {where(reading_time: 2.week.ago .. Time.now)}
   scope :last_month, -> {where(reading_time: 1.month.ago .. Time.now)}
--- cor-dashboard/app/models/physician.rb
+++ cor-dashboard/app/models/physician.rb
@@ -2,5 +2,7 @@ class Physician < ActiveRecord::Base
     attr_accessible :name
     has_many :patients
 
-    
+    if Rails.env.production?
+      self.table_name = "physician"
+    end
 end
