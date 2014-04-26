e627f0c7adcc8575a35d8d08326b4e85bfc1e0c4
sreejitr <sreejitr@andrew.cmu.edu>
2014-04-25 21:53:23 -0400

    Added layout to Patient Roster page, added Patient Activity graph in metrics summary and details page

--- cor-dashboard/app/controllers/physician_controller.rb
+++ cor-dashboard/app/controllers/physician_controller.rb
@@ -33,6 +33,7 @@ class PhysicianController < ApplicationController
 		@bundle[:weight] = @patient.weight
 		@bundle[:sodium] = @patient.sodium
 		@bundle[:blood_pressure] = @patient.blood_pressure
+    @exercise_log = @patient.activity_log[:exercise]
 	end
 
 	def detail
